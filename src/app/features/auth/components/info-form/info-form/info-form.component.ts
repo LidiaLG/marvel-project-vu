c1e254822db3dbe2e7f9564f39c716f7f6ec72c8
LidiaLG <lidialuquegallego91@gmail.com>
2025-06-18 18:42:36 +0200

    upgrate authpage

---
+++ src/app/features/auth/components/info-form/info-form/info-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-form',
+  templateUrl: './info-form.component.html',
+  styleUrl: './info-form.component.scss'
+})
+export class InfoFormComponent {
+
+}
--- src/app/features/auth/module/auth.module.ts
+++ src/app/features/auth/module/auth.module.ts
@@ -5,11 +5,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthComponent } from '../components/auth.component';
 import { AuthViewComponent } from '../auth-view/auth-view.component';
+import { InfoFormComponent } from '../components/info-form/info-form/info-form.component';
 
 
 
 @NgModule({
-  declarations: [AuthComponent, AuthViewComponent],
+  declarations: [AuthComponent, AuthViewComponent, InfoFormComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
